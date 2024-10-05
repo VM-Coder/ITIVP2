ff6f88490ab9af6ae060390509b2fb2f1ddcb46a
Vladimir Makovetskiy <vovamakovetskiy35@gmail.com>
2024-10-05 20:58:46 +0300

    User & admin roles

--- src/pages/profile.php
+++ src/pages/profile.php
@@ -12,18 +12,12 @@
 
     $user = $_SESSION['user'];
 
-    echo '<br>';
-    echo '<div style="text-align: center;">';
-    echo $user->firstname;
-    echo '<br>';
-    echo $user->lastname;
-    echo '</div>';    
-       
     $input_style = "p-4 h-12 bg-slate-100 border-slate-100 focus:border-indigo-500 focus:bg-white outline-none border-2 rounded-lg";
     $button_style = "p-2 h-12 text-white hover:bg-gradient-to-r hover:from-sky-400 hover:to-indigo-400 bg-gradient-to-r from-sky-500 to-indigo-500 rounded-lg";
 ?>
-    
-    <div class="flex flex-col p-12 box-content inset-0 w-3/4 h-fit m-auto gap-4 rounded-lg shadow-lg bg-white">
+
+<?php if ($user->is_admin) : ?>
+        <div class="flex flex-col p-12 box-content inset-0 w-3/4 h-fit m-auto gap-4 rounded-lg shadow-lg bg-white">
         <h1 class="font-bold text-slate-700 text-xl text-center">Взаимодействие с таблицей</h1>
         <hr class="border-slate-700 border-[1px]">
     
@@ -39,13 +33,23 @@
         <p class="text-center text-green-600"><?= !isset($_SESSION['success']) ? '' : $_SESSION['success'] ?></p>
     
         <a href="../authorization" class="text-center hover:underline hover:cursor-pointer text-indigo-500">Выйти</a>
-    </div>
-    
-    <div class="mt-12"> 
-        <h1 class="font-bold text-slate-700 text-xl text-center">Таблица машин</h1>
+        </div>
+        
+        <div class="mt-12"> 
+            <h1 class="font-bold text-slate-700 text-xl text-center">Таблица машин</h1>
 
-        <? include '../ui/table/table_cars.php' ?>
+            <? include '../ui/table/table_cars.php' ?>
+        </div>
+<?php else : ?>
+
+    <br>
+    <div style="text-align: center;">
+     <?= $user->firstname ?>;
+    <br>
+    <?= $user->lastname ?>;
     </div>
+       
+<?php endif ; ?>
     
 <?php
     
